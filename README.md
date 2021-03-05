a47733c27a153b25acf6f5ce17418d77be21848d
Lukas Dirda <lukas.dirda@swedbank.lt>
2021-03-05 08:32:20 +0200

    added loading indication for images

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 Simple demo of infinite scrolling of images. Images are being added dynamically when scrolling is reached the end of items "loaded".
 
-## How to run:
+## How to:
 -  open `index.html` file via browser with `javascript` enabled
+- after scrolling down for a bit you would get a `loading` indication for images 
 -  it works!
--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-var listElm = document.querySelector('#imageContainer');
-
 var maxLoadedItems = 5;
 var nextItem = 1;
+var listElm = document.querySelector('#imageList');
 
-var loadMore = function() {
+var loadMoreImages = function() {
   for (var i = 0; i < maxLoadedItems; i++) {
     var item = document.createElement('img');
-    item.src = 'assets/paris.jpg'
     item.alt = `item # ${nextItem++}`;
+    item.className = 'loading';
+    item.src = 'assets/loading-image.png'
     listElm.appendChild(item);
+    setImageLoaded(item, i);
   }
 }
 
 listElm.addEventListener('scroll', function() {
   if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
-    loadMore();
+    loadMoreImages();
   }
 });
 
-loadMore();
+loadMoreImages();
+
+function setImageLoaded(item, index) {
+  setTimeout(() => {
+    item.src = 'assets/paris.jpg';
+    item.className = '';
+  }, 500 * index)
+}
